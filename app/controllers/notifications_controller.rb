2b4099b87d72bd66aa3bdae6f43cda84bbcc2583
Charlie Smurthwaite <charlie@atechmedia.com>
2018-02-15 15:26:51 +0000

    rails upgrade fixes

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,7 @@
 class NotificationsController < ApplicationController
   
-  before_filter { @application = Application.find(params[:application_id].to_i) }
-  before_filter { params[:id] && @notification = @application.notifications.find(params[:id].to_i) }
+  before_action { @application = Application.find(params[:application_id].to_i) }
+  before_action { params[:id] && @notification = @application.notifications.find(params[:id].to_i) }
   
   def index
     @notifications = @application.notifications.asc.page(params[:page])
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,55 +12,52 @@
 
 ActiveRecord::Schema.define(version: 20140710150626) do
 
-  create_table "applications", force: true do |t|
-    t.string   "name"
-    t.string   "api_key"
+  create_table "applications", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+    t.string "name"
+    t.string "api_key"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "auth_keys", force: true do |t|
-    t.integer  "application_id"
-    t.string   "name"
-    t.string   "key"
+  create_table "auth_keys", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+    t.integer "application_id"
+    t.string "name"
+    t.string "key"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["application_id"], name: "index_auth_keys_on_application_id"
   end
 
-  add_index "auth_keys", ["application_id"], name: "index_auth_keys_on_application_id", using: :btree
-
-  create_table "devices", force: true do |t|
-    t.integer  "application_id"
-    t.string   "token"
-    t.integer  "usage",                     default: 0
+  create_table "devices", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+    t.integer "application_id"
+    t.string "token"
+    t.integer "usage", default: 0
     t.datetime "last_sent_notification_at"
     t.datetime "unsubscribed_at"
     t.datetime "created_at"
     t.datetime "last_registered_at"
-    t.string   "label"
+    t.string "label"
+    t.index ["application_id"], name: "index_devices_on_auth_key_id"
   end
 
-  add_index "devices", ["application_id"], name: "index_devices_on_auth_key_id", using: :btree
-
-  create_table "notifications", force: true do |t|
-    t.integer  "auth_key_id"
-    t.integer  "device_id"
+  create_table "notifications", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+    t.integer "auth_key_id"
+    t.integer "device_id"
     t.datetime "pushed_at"
     t.datetime "created_at"
-    t.text     "data"
-    t.text     "error"
-    t.boolean  "locked",      default: false
+    t.text "data"
+    t.text "error"
+    t.boolean "locked", default: false
+    t.index ["auth_key_id"], name: "index_notifications_on_auth_key_id"
+    t.index ["device_id"], name: "index_notifications_on_device_id"
+    t.index ["pushed_at"], name: "index_notifications_on_pushed_at"
   end
 
-  add_index "notifications", ["auth_key_id"], name: "index_notifications_on_auth_key_id", using: :btree
-  add_index "notifications", ["device_id"], name: "index_notifications_on_device_id", using: :btree
-  add_index "notifications", ["pushed_at"], name: "index_notifications_on_pushed_at", using: :btree
-
-  create_table "users", force: true do |t|
-    t.string   "username"
-    t.string   "password_digest"
-    t.string   "name"
-    t.string   "email_address"
+  create_table "users", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.string "name"
+    t.string "email_address"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
